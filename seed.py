0abdfb1d9ef6f31eb3c99be770db3a904dff11c7
Adam Mwaniki <adammwaniki763@gmail.com>
2026-08-03 11:28:46 +0300

    Implement user authentication and password hashing

--- seed.py
+++ seed.py
@@ -1,6 +1,18 @@
 from app import app, db
+from app.models import User
 
 with app.app_context():
     db.create_all()
+
+    admin = User(
+        username="admin",
+        email="admin@gmail.com",
+        role="admin"
+    )
+
+    admin.set_password("admin123")
+
+    db.session.add(admin)
+    db.session.commit()
 
     print("Database tables created successfully!")
